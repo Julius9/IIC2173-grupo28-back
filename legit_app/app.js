78aebae1d5c7133863314e10122e82f7985083f4
xenon10gh <nmedelcorrea@gmail.com>
2024-04-21 21:25:06 -0400

    solucion arrival y departure

--- legit_app/app.js
+++ legit_app/app.js
@@ -199,15 +199,15 @@ app.get('/flights', async (req, res) => {
             query += ' WHERE departure_airport_id = $1 AND arrival_airport_id = $2 AND departure_airport_time::date = $3';
             queryParams.push(departure, arrival, date);
         }
-        else if (departure && date) {
+        else if (arrival && date) {
             console.log('Filtrar por salida y fecha');
-            query += ' WHERE departure_airport_id = $1 AND departure_airport_time::date = $2';
-            queryParams.push(departure, date);
+            query += ' WHERE arrival_airport_id = $1 AND departure_airport_time::date = $2';
+            queryParams.push(arrival, date);
         }
-        else if (departure) {
+        else if (arrival) {
             console.log('Filtrar por salida');
-            query += ' WHERE departure_airport_id = $1';
-            queryParams.push(departure);
+            query += ' WHERE arrival_airport_id = $1';
+            queryParams.push(arrival);
         }
         else if (date) {
             console.log('Filtrar por fecha');
