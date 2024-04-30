abbc59028216d653926a0deb4d0606951061781c
xenon10gh <nmedelcorrea@gmail.com>
2024-04-30 18:12:24 -0400

    COMIENZO ENTREGA 2

--- auth/src/app.js
+++ auth/src/app.js
@@ -5,11 +5,22 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const authRoutes = require('./authRoutes');
 const errorHandler = require('./errorHandler'); // Asegúrate de crear este middleware
+var cors = require('cors');
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Headers', 'Origin','Accept', 'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Auth'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
 
 dotenv.config(); // Carga las variables de entorno de .env
 
 const app = express();
+app.use(cors(corsOptions));
 
 // Middleware para parsear el cuerpo de las solicitudes JSON
 app.use(bodyParser.json());
