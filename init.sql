2869dc7d33d39b7c452b063ffa86f590e45fdcf2
xenon10gh <nmedelcorrea@gmail.com>
2024-06-23 19:57:46 -0400

    admin en tabla

--- init.sql
+++ init.sql
@@ -34,6 +34,7 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     money DECIMAL(10, 2) NOT NULL DEFAULT 5000.00
+    admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE transaction (
