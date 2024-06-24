8bcc2943d5aa1e7bf53ed94cdd43460db86ba16b
Claudio Bórquez <62002851+claubp99@users.noreply.github.com>
2024-06-24 13:05:37 -0400

    debugging application-stop

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "Stopping application"
-cd /home/ubuntu/IIC2173-grupo28-back 
+cd /home/ubuntu/IIC2173-grupo28-back
+pwd  # Imprime el directorio actual
+ls -l # Lista los archivos en el directorio actual
 docker compose --file docker-compose.production.yml down
