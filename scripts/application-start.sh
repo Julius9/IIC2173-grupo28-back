9732fdad07451544955dd6cd72f2fc5920d2c30c
Claudio Bórquez <claubp99@uc.cl>
2024-06-24 12:41:33 -0400

    CD

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Starting application"
+cd /home/ubuntu/IIC2173-grupo28-back 
 docker compose --file docker-compose.production.yml up -d
--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Stopping application"
+cd /home/ubuntu/IIC2173-grupo28-back 
 docker compose --file docker-compose.production.yml down
